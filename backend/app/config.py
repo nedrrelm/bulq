cca767e8459441795a63fe51bdb473d3f2ac0338
K <a@b.com>
2025-10-10 16:46:50 +0200

    Use repo mode env variable instead of function

--- backend/app/config.py
+++ backend/app/config.py
@@ -25,7 +25,3 @@ MAX_ACTIVE_RUNS_PER_GROUP = int(os.getenv("MAX_ACTIVE_RUNS_PER_GROUP", "100"))
 MAX_PRODUCTS_PER_RUN = int(os.getenv("MAX_PRODUCTS_PER_RUN", "100"))
 MAX_GROUPS_PER_USER = int(os.getenv("MAX_GROUPS_PER_USER", "100"))
 MAX_MEMBERS_PER_GROUP = int(os.getenv("MAX_MEMBERS_PER_GROUP", "100"))
-
-def get_repo_mode() -> str:
-    """Get current repository mode."""
-    return REPO_MODE
