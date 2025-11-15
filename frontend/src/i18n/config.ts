37743d6d64aba5708e3bdf5ff8d425b49b5be5a5
K <a@b.com>
2025-11-15 01:43:59 +0100

    Add russian and serbian translations

--- frontend/src/i18n/config.ts
+++ frontend/src/i18n/config.ts
@@ -20,11 +20,31 @@ import notificationsEn from './locales/en/notifications.json'
 import errorsRu from './locales/ru/errors.json'
 import successRu from './locales/ru/success.json'
 import commonRu from './locales/ru/common.json'
+import authRu from './locales/ru/auth.json'
+import groupsRu from './locales/ru/groups.json'
+import groupRu from './locales/ru/group.json'
+import runRu from './locales/ru/run.json'
+import shoppingRu from './locales/ru/shopping.json'
+import profileRu from './locales/ru/profile.json'
+import storeRu from './locales/ru/store.json'
+import productRu from './locales/ru/product.json'
+import adminRu from './locales/ru/admin.json'
+import notificationsRu from './locales/ru/notifications.json'
 
 // Import Serbian translation files
 import errorsSr from './locales/sr/errors.json'
 import successSr from './locales/sr/success.json'
 import commonSr from './locales/sr/common.json'
+import authSr from './locales/sr/auth.json'
+import groupsSr from './locales/sr/groups.json'
+import groupSr from './locales/sr/group.json'
+import runSr from './locales/sr/run.json'
+import shoppingSr from './locales/sr/shopping.json'
+import profileSr from './locales/sr/profile.json'
+import storeSr from './locales/sr/store.json'
+import productSr from './locales/sr/product.json'
+import adminSr from './locales/sr/admin.json'
+import notificationsSr from './locales/sr/notifications.json'
 
 // Translation resources
 const resources = {
@@ -47,11 +67,31 @@ const resources = {
     errors: errorsRu,
     success: successRu,
     common: commonRu,
+    auth: authRu,
+    groups: groupsRu,
+    group: groupRu,
+    run: runRu,
+    shopping: shoppingRu,
+    profile: profileRu,
+    store: storeRu,
+    product: productRu,
+    admin: adminRu,
+    notifications: notificationsRu,
   },
   sr: {
     errors: errorsSr,
     success: successSr,
     common: commonSr,
+    auth: authSr,
+    groups: groupsSr,
+    group: groupSr,
+    run: runSr,
+    shopping: shoppingSr,
+    profile: profileSr,
+    store: storeSr,
+    product: productSr,
+    admin: adminSr,
+    notifications: notificationsSr,
   },
 }
 
